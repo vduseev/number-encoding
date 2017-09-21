f9acd3da0fe4a5e271c1622669a745217e3ca1b7
vduseev <vagiz.d@gmail.com>
2017-09-21 23:30:15 +0200

    Added argument parser to main script.

--- encode.py
+++ encode.py
@@ -1,15 +1,44 @@
+import argparse
 import configparser
 from phone_numbers_encoder import encode_phone_numbers_file
 from encoding_scheme_builder import build_encoding
 
 
 if __name__ == '__main__':
-    dictionary_path = 'dictionary.txt'
-    phone_numbers_path = 'input.txt'
+    # Default paths to read data from
+    default_dictionary_path = 'docs/requirements/test_dictionary.txt'
+    default_input_path = 'docs/requirements/test_input.txt'
+    default_config_path = 'encoder_config.ini'
+
+    # Set up argument parser for the script
+    parser = argparse.ArgumentParser(description='Encode phone numbers.')
+    parser.add_argument(
+        '-i', '--input',
+        type=str,
+        dest='input_path',
+        default=default_input_path,
+        help='path to input file with phone numbers'
+    )
+    parser.add_argument(
+        '-d', '--dictionary',
+        type=str,
+        dest='dictionary_path',
+        default=default_dictionary_path,
+        help='path to dictionary file with words that will be used for encoding'
+    )
+    parser.add_argument(
+        '-c', '--config',
+        type=str,
+        dest='config_path',
+        default=default_config_path,
+        help='path to config file with digit to char mapping and '
+             'sets of characters that will be ignored during encoding'
+    )
+    args = parser.parse_args()
 
     # Read config file
     config = configparser.ConfigParser()
-    config.read('encoder_config.ini')
+    config.read(args.config_path)
     config_list_separator = ', '
 
     # Get digit to character mapping from config
@@ -24,7 +53,11 @@ if __name__ == '__main__':
 
     # Read dictionary file with words and map each word to the corresponding
     # digit string using digit to character mapping
-    encoding = build_encoding(dictionary_path, mapping, dict_ignored_chars)
+    encoding = build_encoding(
+        args.dictionary_path,
+        mapping,
+        dict_ignored_chars
+    )
 
     # Get list of characters that will be ignored in phone number during
     # encoding process
@@ -37,7 +70,7 @@ if __name__ == '__main__':
     # trailing spaces are not allowed.
     # (as per numberencoding.txt requirements)
     for number_and_encoding in encode_phone_numbers_file(
-            input_path=phone_numbers_path,
+            input_path=args.input_path,
             encoding=encoding,
             ignored_chars=phone_number_ignored_chars):
 
